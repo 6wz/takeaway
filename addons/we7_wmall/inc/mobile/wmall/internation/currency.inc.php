24fe220f472f4bf95afdb7cc29f99fc5dc207396
刘伟忠 <lwz@qq.com>
2023-03-10 22:12:50 +0800

    货币兑换

--- addons/we7_wmall/inc/mobile/wmall/internation/currency.inc.php
+++ addons/we7_wmall/inc/mobile/wmall/internation/currency.inc.php
@@ -3,9 +3,9 @@ defined("IN_IA") or exit( "Access Denied" );
 global $_W;
 global $_GPC;
 $ta = (trim($_GPC["ta"]) ? trim($_GPC["ta"]) : "switch");
-
+$oldCurrency = $_W['currency'] ;
 if($ta == 'switch'){
-    $oldCurrency = $_W['currency'] ;
+
     if($oldCurrency == 'RMB') {
         $newCurrency = 'THB' ;
     }else{
@@ -14,5 +14,9 @@ if($ta == 'switch'){
     setcookie('currency' , $newCurrency) ;
     $_W['currency'] = $newCurrency ;
     imessage(error(0, "success"), "", "ajax");
+}else if($ta = 'exchange') {
+    $value = $_GPC['value'] ;
+    $newValue = exchange($value) ;
+    die(json_encode(error(0 , $newValue))) ;
 }
 
